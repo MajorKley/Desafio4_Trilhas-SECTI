4bb1a878c169d37bad44ced1852f3b6f9985e3c3
brvcelose <contatobruce90@gmail.com>
2025-05-21 07:25:12 -0300

    environment variables update

--- backend/src/main/java/desafio4/security/SecurityConstants.java
+++ backend/src/main/java/desafio4/security/SecurityConstants.java
@@ -8,7 +8,8 @@ import java.util.Base64;
 public class SecurityConstants {
     // Converte a chave Base64 para SecretKey
     public static final SecretKey KEY = Keys.hmacShaKeyFor(
-            Base64.getDecoder().decode("rlMT7aEZSePCUiVpkMBZLr0vtrCPB6uuDNpKQ2AoHgMoUir/xm7PwvXlgqDmVLR4//+yADBCM9savCMKUCxaAw==")
+            //Base64.getDecoder().decode("rlMT7aEZSePCUiVpkMBZLr0vtrCPB6uuDNpKQ2AoHgMoUir/xm7PwvXlgqDmVLR4//+yADBCM9savCMKUCxaAw==")
+            Base64.getDecoder().decode(System.getenv("JWT_SECRET_KEY"))
     );
     public static final String HEADER_NAME = "Authorization";
     public static final Long EXPIRATION_TIME = 1000L * 60 * 30; // 30 minutos
