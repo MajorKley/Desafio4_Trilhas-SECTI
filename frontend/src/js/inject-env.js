5ac2271892cd1a4c181b046028e27a8f0ddf5237
brvcelose <contatobruce90@gmail.com>
2025-05-21 17:11:34 -0300

    deploy with env variables test

--- frontend/src/js/inject-env.js
+++ frontend/src/js/inject-env.js
@@ -1,6 +1,11 @@
 const fs = require('fs');
 const path = require('path');
 
+// Valida se as variáveis existem
+if (!process.env.API_URL || !process.env.GOOGLE_MAPS_API_KEY) {
+    throw new Error("Variáveis de ambiente faltando!");
+}
+
 const configContent = `
 window.APP_CONFIG = {
   API_URL: "${process.env.API_URL}",
