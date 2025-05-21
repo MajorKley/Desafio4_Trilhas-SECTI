1bf76d9eb0774c6a43dfbf01f55acca7b271a910
brvcelose <contatobruce90@gmail.com>
2025-05-21 17:02:34 -0300

    deploy with env variables test

--- frontend/src/js/inject-env.js
+++ frontend/src/js/inject-env.js
@@ -1,17 +1,14 @@
 const fs = require('fs');
 const path = require('path');
 
-const env = {
-    API_URL: process.env.API_URL,
-    GOOGLE_MAPS_API_KEY: process.env.GOOGLE_MAPS_API_KEY
-};
-
-const configPath = path.join(__dirname, 'config.js');
 const configContent = `
 window.APP_CONFIG = {
-  API_URL: "${env.API_URL}",
-  GOOGLE_MAPS_API_KEY: "${env.GOOGLE_MAPS_API_KEY}"
+  API_URL: "${process.env.API_URL}",
+  GOOGLE_MAPS_API_KEY: "${process.env.GOOGLE_MAPS_API_KEY}"
 };
 `;
 
-fs.writeFileSync(configPath, configContent);
+fs.writeFileSync(
+    path.join(__dirname, '../js/config.js'),
+    configContent
+);
